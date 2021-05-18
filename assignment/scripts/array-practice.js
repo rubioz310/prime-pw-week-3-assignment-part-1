4bbbbb2230f573d625b339a2a0d4ce4fe5e3e442
Jose Rubio <ru310z@gmail.com>
2021-05-18 13:22:27 -0500

    Question three answered plus spelling

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -31,15 +31,15 @@ console.log('--- 3. Accessing items in an array ---');
 console.log('First animal is', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array
-
+console.log('Second favorite food is:', favoriteFoods[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index
-
+console.log('Last but not least favorite food is:', favoriteFoods[4]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length,
 //      instead of the exact index number of the last item
-
-
+let foodLength = favoriteFoods.length;
+console.log('Last but not least favorite food is:', favoriteFoods[foodLength-1]);
 
 // 4. Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
